1d3c318b7708721e07e6eb1a26424d2fceb3ea08
phsanzio <phsanzio@outlook.com>
2024-10-04 15:42:39 -0300

    finished

--- MAPcaching.py
+++ MAPcaching.py
@@ -1,18 +1,29 @@
 import math
 
 def hit_or_miss(array_list):
+    #Linha 1 da entrada
     tamanho_ram = int(array_list[0][0])
+    #Linha 2 da entrada
     n_palavras = int(array_list[1][0])
+    #Tamanho da palavra = 4 bytes
     tamanho_bloco = n_palavras * 4
+    #Linha 3 da entrada
     n_linhas = int(array_list[2][0]) 
+    #Linha 4 da entrada
     n_vias = int(array_list[3][0])
+    n_conjuntos = n_linhas / n_vias
+    #Após a linha 4 da entrada
     list_end = array_list[4]
+    #Simula a cache
     list_cache = []
     miss = 0
     hit = 0
-    #print(end)
+
     n_bits_end = int(math.log2(tamanho_bloco))
-    print(n_bits_end)
+    n_bits_conj = int(math.log2(n_conjuntos))
+    n_bits_tag = int(math.log2(tamanho_ram)) - (n_bits_end + n_bits_conj)
+    
+    #Verfica se há o bloco na cache, se houver HIT, caso não MISS
     for end in list_end:
         n_bloco = int(end) // tamanho_bloco
         if n_bloco not in list_cache:
@@ -20,9 +31,11 @@ def hit_or_miss(array_list):
             miss += 1
         else:
             hit += 1
-    #print(list_cache)
-    print(miss)
-    print(hit)
+
+    #Resultado: n_bits_end, n_bits_conj, n_bits_tag, miss, hit
+    array_result = [n_bits_end, n_bits_conj, n_bits_tag, miss, hit]
+    return array_result
+    
     
 
 def text_generator(read_file, write_file):
@@ -40,17 +53,15 @@ def text_generator(read_file, write_file):
                     array_temp.append(element)
                 array_list.append(array_temp)
         
-        #Seleção dos metódos
-        hit_or_miss(array_list)
+
     #Colocar instruções no txt de resultado
-        #print(array_list)
-        # for line in array_list:
-        #     line_printable = " ".join(line)
-        #     write_file.write(line_printable + '\n')
+        for line in hit_or_miss(array_list):
+            #line_printable = " ".join(line)
+            write_file.write(str(line) + '\n')
 
 
 #Mude a quantidade de arquivos
-n_arquivos = 1
+n_arquivos = 4
 #Coloque o caminho exato do arquivo, ex: pc/documentos/testes/
 caminho_arquivo = "testes/"
 for i in range(0, n_arquivos):
